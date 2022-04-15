7bd41ff05f4fa749c876de32ecebd249d021808b
lcthang22 <dttmcndy.lct@gmail.com>
2022-04-16 03:45:11 +0700

    update

---
+++ Card khóa học.rb
@@ -0,0 +1,40 @@
+Card khóa học 
+
+div.card
+    div.image
+            img 
+    div.content
+        h2
+        p.description
+        p.ration
+              span
+
+Card khai giảng       
+
+div.card
+    div.time
+        year
+        datetime
+             date 
+             month
+    div.info
+        title
+        time
+           timetable
+           modal         
+
+Mẫu
+
+div.card
+   div.year
+       h2
+    div.date
+       h2
+       p
+   div.content
+       h2
+       div.info
+          p -> span
+          p -> span
+          p -> span
+        a || button         
